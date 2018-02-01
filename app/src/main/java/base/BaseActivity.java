87acbd08f575d46d030655e58d084e9cfacb06a5
15210183057@163.com <15210583057@163.com>
2018-02-01 12:43:20 +0800

    修改title 颜色

--- app/src/main/java/base/BaseActivity.java
+++ app/src/main/java/base/BaseActivity.java
@@ -5,6 +5,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.Window;
 
+import com.example.a123456.zhonggu.R;
+
 import utils.StatusBarCompat;
 
 public class BaseActivity extends AppCompatActivity {
@@ -18,6 +20,8 @@ public class BaseActivity extends AppCompatActivity {
 //        requestWindowFeature(Window.FEATURENOTITLE)
         supportRequestWindowFeature(Window.FEATURE_NO_TITLE);
         super.onCreate(savedInstanceState);
-        StatusBarCompat.compat(this, Color.RED);
+
+//        StatusBarCompat.compat(this,Color.parseColor("#D60C18"));//设置状态栏颜色
+          StatusBarCompat.compat(this,getResources().getColor(R.color.titleBar));
     }
 }
