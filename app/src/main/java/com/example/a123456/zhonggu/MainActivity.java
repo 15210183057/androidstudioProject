ec36c8af15f79e8a20fe29186efd66b153d40662
15210183057@163.com <15210583057@163.com>
2018-02-08 10:10:08 +0800

    分支

--- app/src/main/java/com/example/a123456/zhonggu/MainActivity.java
+++ app/src/main/java/com/example/a123456/zhonggu/MainActivity.java
@@ -26,6 +26,7 @@ public class MainActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //分支提交
         findViewById(R.id.HelloTV).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
